b29d0c62edc815da402a793b7ae2ad24700187aa
Spirillen <pdns@protonmail.com>
2023-01-18 18:01:55 +0100

    updated index page

--- _public/index.php
+++ _public/index.php
@@ -3,21 +3,12 @@
 
     <head prefix="og: http://ogp.me/ns#">
         <meta charset="utf-8">
-        <meta name="generator" content="https://mypdns.org/spirillen">
+        <meta name="generator" content="https://spirillen.github.io">
         <title>My Privacy DNS's blocking rules for Ad-blockers</title>
         <link rel="preload" href="style.css">
-        <link rel="preload"
-            href="/assets/application_utilities_dark-9aab2a3cee6460c807a07e08fd90f1b437bd024468f30154c162755f26705d70.css"
-            as="style" type="text/css">
-        <link rel="preload"
-            href="/assets/application_dark-fb47def5c7bac4103062f03d10bd5448526f50d9b4d162f950808fe25e6a6496.css"
-            as="style" type="text/css">
-        <link rel="preload"
-            href="/assets/highlight/themes/dark-0537c09f077245d346ebbe73ea39b18833487d026751b5d0dd67c2ee82bdf3f9.css"
-            as="style" type="text/css">
         <link rel="stylesheet" href="style.css">
-        <link rel="license" href="https://mypdns.org/my-privacy-dns/adblocker-rules-/blob/master/LICENSE">
-        <link rel="prefetch" href="https://mypdns.org/">
+        <link rel="license" href="https://framagit.org/my-privacy-dns/adblocker-rules-/blob/master/LICENSE">
+        <link rel="prefetch" href="https://framagit.org/">
         <link rel="shortcut icon" type="image/png" href="/uploads/-/system/appearance/favicon/1/Icon___Wordmark.png"
             id="favicon" data-original-href="/uploads/-/system/appearance/favicon/1/Icon___Wordmark.png" />
 
@@ -27,7 +18,7 @@
         class="gl-dark tab-width-4 gl-browser-firefox gl-platform-windows diff-custom-addition-color diff-custom-deletion-color">
         <div class="navbar">
 
-            <a href="https://mypdns.org/my-privacy-dns/adblocker-rules" target="_top">Repository</a>
+            <a href="https://framagit.org/my-privacy-dns/adblocker-rules" target="_top">Repository</a>
             <a href="https://mypdns.org" target="_blank">My Privacy DNS</a>
         </div>
 
@@ -45,26 +36,35 @@
         </p>
 
         <p>
-            <?php
-                // Program to display current page URL.
-                $link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] 
-                            === 'on' ? "https" : "http") . 
-                            "://" . $_SERVER['HTTP_HOST'] . 
-                            $_SERVER['REQUEST_URI'];
-                echo $link;
-            ?>
+            Click here to our list via one of the mirrors
 
-            Click here to <a
-            href="abp:subscribe?location=https://mypdns.org/adblocker-rules/blockrules.txt&title=My%20Privacy%20DNS%20–%20Adblock%20Rules"
-            target="_top" title="Subscibe this list to adblocker">activate</a>
-            in uBlock Origin or You can find the builded list
-            <a href="./blockrules.txt">here</a>
-            
+        <ul>
+            <li>
+                <a href="abp:subscribe?location=https://my-privacy-dns.frama.io/adblocker-rules/blockrules.txt&title=My%20Privacy%20DNS%20–%20Adblock%20Rules&requiresLocation=https://my-privacy-dns.frama.io/adblocker-rules/blockrules.txt&requiresTitle=My%20Privacy%20DNS%20–%20Adblock%20Rules"
+                    target="_top" title="Subscibe this list to adblocker">mypdns.org</a>
+                </p>
+            </li>
+            <li>
+                <a href="abp:subscribe?location=https://mypdns.org/adblocker-rules/blockrules.txt&title=My%20Privacy%20DNS%20–%20Adblock%20Rules&requiresLocation=https://mypdns.org/adblocker-rules/blockrules.txt&requiresTitle=My%20Privacy%20DNS%20–%20Adblock%20Rules"
+                    target="_top" title="Subscibe this list to adblocker">mypdns.org</a>
+                </p>
+            </li>
+            <li>
+                <a href="abp:subscribe?location=https://mypdns.github.io/adblocker-rules/blockrules.txt&title=My%20Privacy%20DNS%20–%20Adblock%20Rules&requiresLocation=https://mypdns.github.io/adblocker-rules/blockrules.txt&requiresTitle=My%20Privacy%20DNS%20–%20Adblock%20Rules"
+                    target="_top" title="Subscibe this list to adblocker">mypdns.github.io</a>
+            </li>
+        </ul>
+        in uBlock Origin or You can find the builded list
+        <a href="./blockrules.txt">here</a>
+        <br />
+        In case the link ain't working, you can right click on it, then select uBlock origin => subscribe to filter
+        list...
         </p>
 
         <p>
-            If you would like to contribute to the list, please do it at this <a
-                href="https://mypdns.org/my-privacy-dns/adblocker-rules">repo</a></p>
+            If you would like to contribute to the list, please do this at <a
+                href="https://framagit.org/my-privacy-dns/adblocker-rules">mypdns.org</a>
+        </p>
         <p>Cheers</p>
 
     </body>
