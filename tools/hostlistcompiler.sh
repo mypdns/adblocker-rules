6f4624d77182d36615cf847a2bfa913419957899
Spirillen <pdns@protonmail.com>
2023-02-14 15:16:44 +0100

    What is pushed to public?

--- tools/hostlistcompiler.sh
+++ tools/hostlistcompiler.sh
@@ -2,10 +2,13 @@
 
 set -e -x
 
+BUILD="$(git rev-parse --short HEAD)"
 GIT_DIR="$(git rev-parse --show-toplevel)"
 
 # export DEBIAN_FRONTEND=noninteractive
 # apt-get install -yqqf npm
+
+git checkout master
 
 npm i -g @adguard/hostlist-compiler
 
@@ -16,3 +19,8 @@ echo "List the first 15 lines"
 echo ""
 head -n 15 _public/blockrules.txt
 echo ""
+
+git add _public/blockrules.txt
+git commit -m "AdBlocker list updated. BuildID $BUILD"
+git pull --rebase
+git push "https://spirillen:${mypdns_CI_ACCESS_TOKEN}@${CI_REPOSITORY_URL#*@}" "HEAD:${CI_COMMIT_REF_NAME}" -o skip-ci
